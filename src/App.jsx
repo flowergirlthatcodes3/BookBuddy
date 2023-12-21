459af3b638537c9e69c080da725903860754d114
flowergirlthatcodes3 <149510282+flowergirlthatcodes3@users.noreply.github.com>
2023-12-21 14:34:33 -0500

    updating

--- src/App.jsx
+++ src/App.jsx
@@ -1,84 +1,105 @@
-import { useState, useEffect } from 'react'
-import axios from 'axios'
-import bookLogo from './books.png'
-import {Routes, Route, Link,} from 'react-router-dom'
-import Books from'./components/Books'
-import SingleBook from './components/SingleBook'
-import Navigations from "./components/Navigations"
-import Login from './components/Login'
-import Register from './components/Register'
-import Account from './components/Account'
-import SuccessRegi from './components/SuccessRegi'
-import Homepage from './components/Homepage'
-import About from './components/AboutUs'
+import { useState, useEffect } from "react";
+import axios from "axios";
+import bookLogo from "./assets/books.png";
+import { Routes, Route, Link } from "react-router-dom";
+import Books from "./components/Books";
+import SingleBook from "./components/SingleBook";
+import Navigations from "./components/Navigations";
+import Login from "./components/Login";
+import Register from "./components/Register";
+import Account from "./components/Account";
+import SuccessRegi from "./components/SuccessRegi";
+import Homepage from "./components/Homepage";
+import About from "./components/AboutUs";
 
 function App() {
-  const [token, setToken] = useState(null)
-  const [user, setUser] = useState({})
-  const [books, setBooks] = useState([])
-
+  const [token, setToken] = useState(null);
+  const [user, setUser] = useState({});
+  const [books, setBooks] = useState([]);
 
   useEffect(() => {
-    const fetchBooks = async() => {
-        const {data} = await axios.get('https://fsa-book-buddy-b6e748d1380d.herokuapp.com/api/books')
-        setBooks(data.books)
-        //* checking data console.log(data)
-      
-    }
-    fetchBooks() 
-  }, [])
-  useEffect(() =>{
-    
-    const attemptLogin = async() => {
-      const loggedInToken = window.localStorage.getItem('token')
-      
+    const fetchBooks = async () => {
+      const { data } = await axios.get(
+        "https://fsa-book-buddy-b6e748d1380d.herokuapp.com/api/books"
+      );
+      setBooks(data.books);
+      //* checking data console.log(data)
+    };
+    fetchBooks();
+  }, []);
+  useEffect(() => {
+    const attemptLogin = async () => {
+      const loggedInToken = window.localStorage.getItem("token");
 
-      if(loggedInToken){
-        const response = await axios.get('https://fsa-book-buddy-b6e748d1380d.herokuapp.com/api/users/me', {
-          headers: {
-            'Content-Type': 'application/json',
-            'Authorization': `Bearer ${loggedInToken}`
+      if (loggedInToken) {
+        const response = await axios.get(
+          "https://fsa-book-buddy-b6e748d1380d.herokuapp.com/api/users/me",
+          {
+            headers: {
+              "Content-Type": "application/json",
+              Authorization: `Bearer ${loggedInToken}`,
+            },
           }
-        })
+        );
 
-        setUser(response.data)
+        setUser(response.data);
         //*console.log(response.data)
-      }else{
-        
-        throw 'no token'
+      } else {
+        throw "no token";
       }
+    };
 
-    }
-    
-    attemptLogin()
-  },[token])
+    attemptLogin();
+  }, [token]);
 
-  
-  
-
- 
   return (
     <>
-    <h1><img id='logo-image' src={bookLogo}/><Link to='/'>Your Library</Link></h1>
-    <Navigations user={user}/>
- 
-   
-    
-    <Routes>
-      <Route path='/' element={<Homepage books={books} user={user} />}/>
-      <Route path='/successReg' element={<SuccessRegi />}/>
-      <Route path='/books' element={<Books books={books} user={user} setUser={setUser} token={token}/>}/>
-      <Route path='/books/:id' element={<SingleBook books={books} user={user} setUser={setUser} token={token}/>}/>
-      <Route path='/login' element={<Login setUser={setUser} setToken={setToken}/>}/>
-      <Route path='/register' element={<Register />}/>
-      <Route path='/account' element={<Account user={user} setUser={setUser} setToken={setToken} books={books}/>}/>
-      <Route path='/about' element={<About books={books} />}/>
+      <h1>
+        <img id="logo-image" src={bookLogo} />
+        <Link to="/">Your Library</Link>
+      </h1>
+      <Navigations user={user} />
 
-    </Routes>
-    
-      
+      <Routes>
+        <Route path="/" element={<Homepage books={books} user={user} />} />
+        <Route path="/successReg" element={<SuccessRegi />} />
+        <Route
+          path="/books"
+          element={
+            <Books books={books} user={user} setUser={setUser} token={token} />
+          }
+        />
+        <Route
+          path="/books/:id"
+          element={
+            <SingleBook
+              books={books}
+              user={user}
+              setUser={setUser}
+              token={token}
+            />
+          }
+        />
+        <Route
+          path="/login"
+          element={<Login setUser={setUser} setToken={setToken} />}
+        />
+        <Route path="/register" element={<Register />} />
+        <Route
+          path="/account"
+          element={
+            <Account
+              user={user}
+              setUser={setUser}
+              setToken={setToken}
+              books={books}
+            />
+          }
+        />
+        <Route path="/about" element={<About books={books} />} />
+      </Routes>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
